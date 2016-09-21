85897aece23c599e04492427ea0e6bf479e8fc9c
masatoko <masatokodo@gmail.com>
2016-09-21 12:37:24 +0900

    Main with SDL

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Linear.Affine
-import Linear.V2
+-- import Linear.Affine
+-- import Linear.V2
+import SDL.Vect
+
+import SDL (($=))
+import qualified SDL
 
 import Amaterasu
 import Type
 
 main :: IO ()
-main = test
+main = do
+  SDL.initializeAll
+  win <- SDL.createWindow "Amaterasu" SDL.defaultWindow {SDL.windowInitialSize = V2 600 600}
+  SDL.showWindow win
 
-test :: IO ()
-test =
+  --
+  renderer <- SDL.createRenderer win 0 SDL.defaultRenderer
+  SDL.rendererDrawBlendMode renderer $= SDL.BlendAlphaBlend
+  SDL.clear renderer
+  --
+
+  test renderer
+
+  SDL.destroyWindow win
+  SDL.quit
+
+test :: SDL.Renderer -> IO ()
+test rnd = do
   -- makeFieldOfView eye ps boundary
+  SDL.present rnd
+  SDL.delay 1000
   return ()
   where
     eye = P $ V2 400 300
-    boundary = Rect (pure 0) (V2 800 600)
+    boundary = Rect (pure 0) (V2 600 600)
     ps = [p1]
     p1 = map P [V2 350 50, V2 500 100, V2 500 200, V2 350 250]
